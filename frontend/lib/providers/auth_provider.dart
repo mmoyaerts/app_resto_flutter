e1d05d03e9b10b371e6fd750ffe23eb34334b4fa
thomas <tletoubgym74@gmail.com>
2025-11-07 11:51:44 +0100

    connexion okay

--- frontend/lib/providers/auth_provider.dart
+++ frontend/lib/providers/auth_provider.dart
@@ -4,17 +4,23 @@ import '../service/auth_service.dart';
 class AuthProvider extends ChangeNotifier {
   bool _isLoggedIn = false;
   String? _token;
+  int? _role;
+  int? _id;
 
   bool get isLoggedIn => _isLoggedIn;
   String? get token => _token;
+  int? get role => _role;
+  int? get id => _id;
 
   final AuthService _authService = AuthService();
 
   /// Connexion
   Future<bool> login(String email, String password) async {
-    final token = await _authService.login(email, password);
-    if (token != null) {
-      _token = token;
+    final response = await _authService.login(email, password);
+    if (response != null) {
+      _token = response['token'];
+      _role = response['role'];
+      _id = response['id'];
       _isLoggedIn = true;
       notifyListeners();
       return true;
